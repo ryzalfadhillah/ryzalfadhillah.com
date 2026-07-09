d051d7e450d5a47896f7e6dc1a7e2db963615d67
15997683 <ryzal.fadhillah@muf.co.id>
2026-07-09 10:44:30 +0700

    feat: successfully configure tailwind v4 and sanity integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,18 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
+import sanity from '@sanity/astro';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()], // Konfigurasi untuk Tailwind v4
+  },
+  integrations: [
+    sanity({
+      projectId: 'mxkcjxob', // <--- Ganti dengan Project ID dari Sanity
+      dataset: 'production',
+      useCdn: true,
+      studioUrl: '/admin', // Supaya dashboard CMS bisa diakses di localhost:4321/admin
+    }),
+  ],
+});

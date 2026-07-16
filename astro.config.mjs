8b24645454fbaec56d0b563facc47adce7afa4b7
15997683 <ryzal.fadhillah@muf.co.id>
2026-07-16 15:35:03 +0700

    feat: configure site url and automatic sitemap integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,14 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwindcss from '@tailwindcss/vite';
+import sitemap from '@astrojs/sitemap'; 
 
 export default defineConfig({
-  integrations: [react()], 
+  site: 'https://ryzalfadhillah.tech', 
+    integrations: [
+    react(), 
+    sitemap()
+  ], 
   vite: {
     plugins: [tailwindcss()],
     optimizeDeps: {
